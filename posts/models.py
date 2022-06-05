cca018a3588516d6fd0c79a9c6d75a536aa32b40
sirgama <sirengogamaliel@gmail.com>
2022-06-06 00:52:58 +0300

    create a tag model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,34 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save, post_delete
+from django.utils.text import slugify
+from django.urls import reverse
+import uuid
 
 # Create your models here.
+
+
+#uploading user files to a specific directory
+def user_directory_path(instance, filename):
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
+
+class Tag(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Tag")
+    slug = models.SlugField(null=False, unique=True, default=uuid.uuid1)
+    
+    class Meta:
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tags'
+    
+    def get_abolute_url(self):
+        return reverse('tags', args=[self.slug])
+    
+    def __str__(self):
+        return self.title
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug - slugify(self.slug)
+        return super().save(*args, **kwargs)
+    
+    
