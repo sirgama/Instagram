f55c3cedccb1e169bd7f4d4458d5a21d7848aa0a
sirgama <sirengogamaliel@gmail.com>
2022-06-05 17:08:31 +0300

    create a user update model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics', null=True)
+    first_name = models.CharField(max_length=200, null=True, blank=True)
+    last_name = models.CharField(max_length=200, null=True, blank=True)
+    bio = models.CharField(max_length=200, null=True, blank=True)
+    location = models.CharField(max_length=200, null=True, blank=True)
+    url = models.URLField(max_length=200, null=True, blank=True)
+    
+    
+    def __str__(self):
+        return f'{self.user.username} Profile'
