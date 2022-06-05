4256d352f0aa926978cb5889b0d59898d2463498
sirgama <sirengogamaliel@gmail.com>
2022-06-05 17:21:20 +0300

    add functin to reduce profile image size

--- users/models.py
+++ users/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import PIL
+from PIL import Image
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -14,3 +16,12 @@ class Profile(models.Model):
     
     def __str__(self):
         return f'{self.user.username} Profile'
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
