0b98694b865ce8bfb03c8ee3e88e8db60973ea36
sirgama <sirengogamaliel@gmail.com>
2022-06-05 22:44:49 +0300

    create edit account view function and url pattern

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 # Create your views here.
 
@@ -23,4 +23,18 @@ def register(request):
 @login_required
 def profile(request):
     
+    
     return render(request, 'users/profile.html')
+
+@login_required
+def edit_account(request):
+    
+    u_form = UserUpdateForm()
+    p_form = ProfileUpdateForm()
+    
+    context = {
+        'u_form' : u_form,
+        'p_form' : p_form
+    }
+    
+    return render(request, 'users/edit_account.html')
