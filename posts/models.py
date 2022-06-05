3fea844d335caf736e1bff8bd582e661bfbddede
sirgama <sirengogamaliel@gmail.com>
2022-06-06 01:17:10 +0300

    create a follow model

--- posts/models.py
+++ posts/models.py
@@ -47,5 +47,9 @@ class Post(models.Model):
     def __str__(self):
         return self.caption
     
+class Follow(models.Model):
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follower')
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
+    
     
     
