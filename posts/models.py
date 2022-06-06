3ae97ebd503887ba2946faa240a8c8905433668c
sirgama <sirengogamaliel@gmail.com>
2022-06-06 10:30:01 +0300

    add posts to feed

--- posts/models.py
+++ posts/models.py
@@ -20,40 +20,39 @@ class Tag(models.Model):
     class Meta:
         verbose_name = 'Tag'
         verbose_name_plural = 'Tags'
-    
-    def get_abolute_url(self):
+
+    def get_absolute_url(self):
         return reverse('tags', args=[self.slug])
-    
+
     def __str__(self):
         return self.title
+
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug - slugify(self.slug)
+            self.slug = slugify(self.title)
         return super().save(*args, **kwargs)
     
 
 class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    picture = models.ImageField(upload_to=user_directory_path, verbose_name='Picture', null=True)
-    caption = models.CharField(max_length=1000000, verbose_name="Caption")
-    posted = models.DateTimeField(auto_now_add=True)
-    tag = models.ManyToManyField(Tag, related_name='tags')
+    picture = models.ImageField(upload_to=user_directory_path, verbose_name="Picture", blank=True)
+    caption = models.CharField(max_length=10000, verbose_name="Caption")
+    posted = models.DateField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag, related_name="tags")
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     likes = models.IntegerField(default=0)
+
+    def get_absolute_url(self):
+        return reverse("post-details", args=[str(self.id)])
     
-    def get_abolute_url(self):
-        return reverse('post-details', args=[str(self.id)])
-    
-    def __str__(self):
-        return self.caption
-    
+   
 class Follow(models.Model):
     follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follower')
     following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
     
     
 class Stream(models.Model):
-    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='stream_following')
+    following = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='stream_following')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='stream_user')
     post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True)
     date = models.DateTimeField()
@@ -62,8 +61,9 @@ class Stream(models.Model):
         post = instance
         user = post.user
         followers = Follow.objects.all().filter(following=user)
+
         for follower in followers:
-            stream = Stream(post=post, user=follower, date=post.posted, following=user)
+            stream = Stream(post=post, user=follower.follower, date=post.posted, following=user)
             stream.save()
             
 post_save.connect(Stream.add_post, sender=Post)
--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Tag, Post, Follow, Stream
 
 # Create your views here.
 
 @login_required
 def home(request):
-    
-    return render(request, 'posts/home.html')
+    user = request.user
+    # posts = Stream.objects.filter(user=user)
+    # group_ids = []
+    # for post in posts:
+    #     group_ids.append(post.post.id)
+    post_items = Post.objects.all()
+    post_items.reverse()
+    context = {
+        'post_items': post_items
+    }
+    return render(request, 'posts/home.html', context)
